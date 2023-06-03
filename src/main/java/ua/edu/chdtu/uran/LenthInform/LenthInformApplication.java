c00281d870aae86000d597b76b0cef60d4b25b6b
Роман <evilrk666@gmail.com>
2023-06-03 18:44:13 +0300

    test

--- src/main/java/ua/edu/chdtu/uran/LenthInform/LenthInformApplication.java
+++ src/main/java/ua/edu/chdtu/uran/LenthInform/LenthInformApplication.java
@@ -8,6 +8,7 @@ public class LenthInformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LenthInformApplication.class, args);
+		System.out.println("ujuj");
 	}
 
 }
