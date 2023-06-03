4acdb053ecb8dbe2810706ad3972e4c08bc23101
Роман <evilrk666@gmail.com>
2023-06-03 18:47:01 +0300

    removed xujnia

--- src/main/java/ua/edu/chdtu/uran/LenthInform/LenthInformApplication.java
+++ src/main/java/ua/edu/chdtu/uran/LenthInform/LenthInformApplication.java
@@ -8,7 +8,7 @@ public class LenthInformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LenthInformApplication.class, args);
-		System.out.println("ujuj");
+
 	}
 
 }
