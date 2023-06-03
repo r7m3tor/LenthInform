3517b4c1262340ae7623a7eb5470495bee73c14d
Khimichenko Vladislav <khimichenko.vladislav@gmail.com>
2023-06-03 18:33:24 +0300

    Initial project

---
+++ src/main/java/ua/edu/chdtu/uran/LenthInform/LenthInformApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.chdtu.uran.LenthInform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LenthInformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LenthInformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/chdtu/uran/LenthInform/LenthInformApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.chdtu.uran.LenthInform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenthInformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
