5787aff181e21d2d90a15df2094b4a7c0c30132f
Khimichenko Vladislav <khimichenko.vladislav@gmail.com>
2023-06-10 20:38:36 +0300

    Added order last 6 news

--- src/main/java/ua/edu/chdtu/uran/LenthInform/ServiceController.java
+++ src/main/java/ua/edu/chdtu/uran/LenthInform/ServiceController.java
@@ -1,15 +1,15 @@
 package ua.edu.chdtu.uran.LenthInform;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import ua.edu.chdtu.uran.LenthInform.db.NewsRepository;
 import ua.edu.chdtu.uran.LenthInform.model.News;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -22,20 +22,24 @@ public class ServiceController {
     }
 
     @RequestMapping("/addNew")
-    @Transactional
     @PutMapping
-    public Mono<News> addNew(HttpSession httpSession, @RequestBody News news) {
+    public String addNew(HttpSession httpSession, @RequestBody News news) {
         if (httpSession.getAttribute("Login") != null) {
-            return repository.save(news);
+            repository.save(news).block();
         } else {
             throw new RuntimeException("Need login");
         }
+        return "{}";
     }
 
     @RequestMapping("/getLast")
-    @Transactional
-    public Flux<News> getLast(HttpSession httpSession) {
+    public List<News> getLast(HttpSession httpSession) {
         httpSession.setAttribute("Login", "DAKARTA");
-        return repository.findAll();
+        return repository
+                .findAll()
+                .toStream()
+                .sorted((o1, o2) -> o2.getId().compareTo(o1.getId()))
+                .limit(6)
+                .collect(Collectors.toList());
     }
 }
